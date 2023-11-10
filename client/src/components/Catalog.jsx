8e189a88a980f279d65ae1f1c067c2c5c39fc567
Georgi St. Lukanov <goshiii87@gmail.com>
2023-11-10 18:22:01 +0200

    Update Catalog.jsx

--- client/src/components/Catalog.jsx
+++ client/src/components/Catalog.jsx
@@ -1,8 +1,9 @@
 import React, { useContext, useState } from 'react';
-import SearchBar from './SearchBar';
-import CatalogItem from './CatalogItem';
 import { Link } from "react-router-dom";
 import { AuthContext } from '../contexts/AuthContext';
+
+import SearchBar from './SearchBar';
+import CatalogItem from './CatalogItem';
 
 const Catalog = ({ motos }) => {
     const [searchTerm, setSearchTerm] = useState('');
@@ -46,40 +47,3 @@ const Catalog = ({ motos }) => {
 };
 
 export default Catalog;
-
-
-
-
-
-
-// import CatalogItem from "./CatalogItem";
-// import { Link } from "react-router-dom";
-// import { useContext } from "react";
-// import { AuthContext } from "../contexts/AuthContext";
-
-// const Catalog = (props) => {
-//     const { user } = useContext(AuthContext);
-
-//     return (
-//         <section id="catalog-page">
-
-//             {props.motos?.length > 0
-//                 ? <h1>All Motos</h1>
-//                 : <h3 className="no-articles">No articles yet</h3>
-//             }
-
-//             {props.motos.map(x => <CatalogItem key={x._id} moto={x} />)}
-
-
-//             {user &&
-//                 <button className="btn submit"><Link to={'/create'}>Add moto</Link></button>
-
-//             }
-
-//             <button className="btn submit"><Link to={'/'}>Back</Link></button>
-
-//         </section>
-//     );
-// }
-
-// export default Catalog;
