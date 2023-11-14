08aa40c6176d447e745dae2d7a3f0aec27fd24c5
Georgi St. Lukanov <goshiii87@gmail.com>
2023-11-14 16:47:23 +0200

    Update CatalogItem.jsx

--- client/src/components/CatalogItem.jsx
+++ client/src/components/CatalogItem.jsx
@@ -1,26 +1,24 @@
 import { Link } from "react-router-dom";
 
-const CatalogItem = (props) => {
-
-    console.log(props)
+const CatalogItem = ({moto}) => {
 
     return (
         <div className="allMotos">
             <div className="allMotos-info">
 
-                {props.moto.imageUrl
-                    ? <img src={props.moto.imageUrl} alt="not-alt" />
+                {moto.imageUrl
+                    ? <img src={moto.imageUrl} alt="not-alt" />
                     : <img src="https://www.freeiconspng.com/thumbs/no-image-icon/no-image-icon-15.png" alt="not-alt" />
                 }
 
-                <h6>Model: {props.moto.model}</h6>
-                <h2>Brand: {props.moto.brand}</h2>
-                <h2>cc:{props.moto.cc}</h2>
-                <h2>Price:{props.moto.price}$</h2>
+                <h6>Model: {moto.model}</h6>
+                <h2>Brand: {moto.brand}</h2>
+                <h2>cc:{moto.cc}</h2>
+                <h2>Price:{moto.price}$</h2>
                 <br />
-                <h5>Available from:{props.moto.createdAt}</h5>
+                <h5>Available from:{moto.createdAt}</h5>
 
-                <Link to={`/catalog/${props.moto._id}`} className="details-button">
+                <Link to={`/catalog/${moto._id}`} className="details-button">
                     Details
                 </Link>
             </div>
