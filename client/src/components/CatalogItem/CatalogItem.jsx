03cf67fa419124a700796ce5e86d6e1fcac2ca2c
Georgi St. Lukanov <goshiii87@gmail.com>
2023-12-04 14:10:36 +0200

    catalog item css fix

--- client/src/components/CatalogItem/CatalogItem.jsx
+++ client/src/components/CatalogItem/CatalogItem.jsx
@@ -8,21 +8,31 @@ const CatalogItem = ({ moto }) => {
         <div className={style.allMotos}>
             <div className={style.allMotosInfo}>
 
-                {moto.imageUrl
-                    ? <img src={moto.imageUrl} alt="not-alt" />
-                    : <img src={noImageAvailable} alt="not-alt" />
-                }
-
-                <h6>Model: {moto.model}</h6>
-                <h2>Brand: {moto.brand}</h2>
-                <h2>cc:{moto.cc}</h2>
-                <h2>Price:{moto.price}$</h2>
-                <h5>Available from: {moto.createdAt}</h5>
+            <div>
+                    {moto.imageUrl
+                        ? <img src={moto.imageUrl} alt="not-alt" />
+                        : <img src={noImageAvailable} alt="not-alt" />
+                    }
+                </div>
 
 
-                <Link to={`/catalog/${moto._id}`} className="details-button">
-                    Details
-                </Link>
+                <div>
+                    <h2>Model: {moto.model}</h2>
+                    <h2>Brand: {moto.brand}</h2>
+                    <h2>cc: {moto.cc}</h2>
+                    <h2>Price: {moto.price}$</h2>
+                    <h5>Available from: {moto.createdAt}</h5>
+                </div>
+
+
+                <div>
+                    <Link to={`/catalog/${moto._id}`} className="details-button">
+                        Details
+                    </Link>
+                </div>
+
+
+
             </div>
         </div>
     );
