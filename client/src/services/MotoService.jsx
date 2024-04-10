d876e4d9abee07831f12acc135cc50e2ed45f646
Georgi St. Lukanov <102332504+GeorgiLukanov87@users.noreply.github.com>
2024-04-10 22:26:18 +0300

    Update MotoService.jsx

--- client/src/services/MotoService.jsx
+++ client/src/services/MotoService.jsx
@@ -1,14 +1,12 @@
 import * as request from "./requester";
 
+
 const baseUrl = 'http://localhost:3030/data/motos';
 // const baseUrl = 'https://sell-moto-mania-server.onrender.com/data/motos';
 
 export const getAll = () => request.get(baseUrl);
 
 export const getOne = (motoId) => request.get(`${baseUrl}/${motoId}`);
-
 export const addMoto = (data) => request.post(baseUrl, data);
-
 export const deleteMoto = (motoId) => request.del(`${baseUrl}/${motoId}`);
-
 export const editMoto = (data, motoId) => request.patch(`${baseUrl}/${motoId}`, data);
